4848e686fff068ae4bff30f74cad211a558c944c
Oleksandr Nikiforov <nikiforov.aleksandr@gmail.com>
2023-11-13 03:25:18 -0800

    Added two first lessons

---
+++ Lesson01/Lesson01/Program.cs
@@ -0,0 +1,202 @@
+﻿using System.Text;
+
+namespace Lesson01
+{
+    internal class Program
+    {
+        /*
+        1) Знайти позицію літери в алфавіті та перевести її в інший регістр
+        2) Розділювач рядка. Дана строка та символ, потрібно розділити строку на кілька строк (масив строк) виходячи із заданого символу. 
+            Наприклад: строка = "Лондон, Париж, Рим", а символ = ','. Результат = string[] { "Лондон", "Париж", "Рим" }.
+        3) Пошук підстроки у строці.
+        4) Написати програму, яка виводить число літерами. Приклад: 117 - сто сімнадцять
+        5) Поміняти місцями значення двох змінних (типу int) (без використання 3й)
+    Не потрібно використовувати стандартні рішення (наприклад string.Split у 2му завданні).
+         */
+        static void Main(string[] args)
+        {
+
+            Task1();
+            Console.WriteLine();
+            Task2();
+            Console.WriteLine();
+            Task3();
+            Console.WriteLine();
+            //Task4();
+            Task5();
+
+            Console.ReadKey();
+        }
+        private static void Task5()
+        {
+            Console.WriteLine("Task 5:");
+            Console.WriteLine("Enter a number 1:");
+            var str1 = Console.ReadLine();
+            var num1 = 0;
+            if (string.IsNullOrEmpty(str1) || !int.TryParse(str1, out num1))
+            {
+                Console.WriteLine("String should a number and not be empty!");
+                return;
+            }
+
+            Console.WriteLine("Enter a number 2:");
+
+            var str2 = Console.ReadLine();
+            var num2 = 0;
+            if (string.IsNullOrEmpty(str2) || !int.TryParse(str2, out num2))
+            {
+                Console.WriteLine("String should a number and not be empty!");
+                return;
+            }
+
+            Console.WriteLine("Numbers before swap: {0} and {1}", num1, num2);
+            num1 = num1 + num2;
+            num2 = num1 - num2;
+            num1 = num1 - num2;
+            Console.WriteLine("Numbers after swap: {0} and {1}", num1, num2);
+
+        }
+
+        private static void Task4()
+        {
+            Console.WriteLine("Task 4:");
+            var numbers = new string[] { "", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
+            var tens = new string[] { "", "ten", "twenty", "thirty", "fourty", "fifty", "sixty", "seventy", "eighty","ninety" };
+            var teens = new string[] { "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen","seventeen","eighteen","nineteen" };
+            var ranks = new string[] { "hundred", "thousand", "million", "billion", "trillion" };
+
+            Console.WriteLine("Enter a number:");
+            var str = Console.ReadLine();
+            if (string.IsNullOrEmpty(str))
+            {
+                Console.WriteLine("String should not be empty!");
+                return;
+            }
+            
+            var rank = 0;
+            var result = new StringBuilder();
+            rank = str.Length / 3;
+
+            //for(int i = 0; i < )
+        }
+
+        private static void Task3()
+        {
+            Console.WriteLine("Task 3:");
+            Console.WriteLine("Enter a string:");
+            var str = Console.ReadLine();
+            if (string.IsNullOrEmpty(str))
+            {
+                Console.WriteLine("String should not be empty!");
+                return;
+            }
+
+            Console.WriteLine("Enter a substring:");
+            var substr = Console.ReadLine();
+            if (string.IsNullOrEmpty(substr))
+            {
+                Console.WriteLine("Substring should not be empty!");
+                return;
+            }
+
+            if(substr.Length >= str.Length)
+            {
+                Console.WriteLine("Substring should not be longer than string!");
+                return;
+            }
+
+            int iSub = 0;
+
+            for (int i = 0; i<str.Length; i++)
+            {
+                if (str[i] == substr[iSub])
+                {
+                    if(iSub == substr.Length - 1)
+                    {
+                        Console.WriteLine("Substring found at position {0}", i - substr.Length + 1);
+                        iSub = 0;
+                    }
+                    iSub++;
+                }
+                else
+                {
+                    iSub = 0;
+                }
+            }
+            Console.WriteLine("Substring is not found!");
+        }
+
+        private static void Task2()
+        {
+            Console.WriteLine("Task 2:");
+            Console.WriteLine("Enter a string:");
+            var str = Console.ReadLine();
+            if (string.IsNullOrEmpty(str))
+            {
+                Console.WriteLine("String should not be empty!");
+                return;
+            }
+            Console.WriteLine("Enter a divider:");
+            var div = Console.ReadLine();
+            if (string.IsNullOrEmpty(div) || div.Length != 1)
+            {
+                Console.WriteLine("Divider should 1 character long!");
+                return;
+            }
+
+            Console.WriteLine($"Result: { StringDivider(str, div[0]).Aggregate("", (acc, curr) => $"{acc}, \"{curr}\"") }");
+        }
+
+        // Оскільки у другому завданні написано що є вхідні данні (строка та символ), то я зробив окрему фіункцію з ціми вхідними даними
+        private static string[] StringDivider(string input, char div)
+        {
+            var result = new List<string>();
+            var current = new StringBuilder();
+
+            for (int i = 0; i< input.Length;i++) {
+                // як я зрозумів, у результаті не повинно бути пробілів
+                if (input[i] != div)
+                {
+                    current.Append(input[i]);
+                } 
+                else
+                {
+                    result.Add(current.ToString());
+                    current.Clear();
+                }
+            }
+
+            return result.ToArray();
+        }
+
+        private static void Task1()
+        {
+            Console.WriteLine("Task 1:");
+            Console.WriteLine("Enter a letter:");
+
+            var str = Console.ReadLine();
+            if (str == null || str.Length != 1)
+            {
+                Console.WriteLine("Enter only one letter");
+                return;
+            }
+            char letter = str[0];
+            if (letter >= 65 && letter <= 90)
+            {
+                Console.WriteLine("The letter \"{0}\" number in alphabet - {1}", letter, (letter - 'A' + 1));
+                Console.WriteLine("The letter in lower register - " + (char)(letter + 32));
+                return;
+            }
+            else if (letter >= 97 && letter <= 122)
+            {
+                Console.WriteLine("The letter \"{0}\" number in alphabet - {1}", letter, (letter - 'a' + 1));
+                Console.WriteLine("The letter in upper register - " + (char)(letter - 32));
+                return;
+            }
+            else 
+            {
+                Console.WriteLine("Input is not a letter!");
+            }
+        }
+    }
+}
---
+++ Lesson02/Lesson02/Program.cs
@@ -0,0 +1,333 @@
+﻿using System;
+using System.Net;
+using System.Reflection;
+using System.Text;
+
+namespace Lesson02
+{
+    internal class Program
+    {
+        /*
+        1) Реверс строки/масиву. Без додаткового масиву. Складність О(n).
+        2) Фільтрування неприпустимих слів у строці. Має бути саме слова, а не частини слів.
+        3) Генератор випадкових символів. На вхід кількість символів, на виході рядок з випадковими символами.
+        4) "Дірка" (пропущене число) у масиві. Масив довжини N у випадковому порядку заповнений цілими числами з діапазону від 0 до N. 
+            Кожне число зустрічається в масиві не більше одного разу. Знайти відсутнє число (дірку). 
+            Є дуже простий та оригінальний спосіб вирішення. Складність алгоритму O(N). 
+            Використання додаткової пам'яті, пропорційної довжині масиву не допускається.
+        5) Найпростіше стиснення ланцюжка ДНК. Ланцюг ДНК у вигляді строки на вхід (кожен нуклеотид представлений символом "A", "C", "G", "T"). 
+            Два методи, один для компресії, інший для декомпресії.
+        6) Симетричне шифрування. Є строка на вхід, який має бути зашифрований. Ключ можна задати в коді або згенерувати та зберегти. 
+            Два методи, шифрування та дешифрування.
+        */
+
+        static void Main(string[] args)
+        {
+            Task1();
+            Task2();
+            Task3();
+            Task4();
+            Task5();
+            Task6();
+        }
+
+        public static string Encrypt(string str)
+        {
+            var key = "123Asd";
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < str.Length; i++)
+            {
+                sb.Append((char)(str[i] ^ key[i % key.Length]));
+            }
+            return sb.ToString();
+        }
+
+        public static string Decrypt(string str, string key)
+        {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < str.Length; i++)
+            {
+                sb.Append((char)(str[i] ^ key[i % key.Length]));
+            }
+            return sb.ToString();
+        }
+
+        public static void Task6()
+        {
+            Console.WriteLine("Begin Task 6");
+            Console.WriteLine("Enter string");
+            string str = Console.ReadLine();
+
+            if (str == null)
+            {
+                Console.WriteLine("String is null");
+                return;
+            }
+
+            Console.WriteLine("Enter key");
+            string key = Console.ReadLine();
+
+            if (key == null)
+            {
+                Console.WriteLine("Key is null");
+                return;
+            }
+
+            string encrypted = Encrypt(str);
+            string decrypted = Decrypt(encrypted, key);
+
+            Console.WriteLine("Encrypted:");
+            Console.WriteLine(encrypted);
+
+            Console.WriteLine("Decrypted:");
+            Console.WriteLine(decrypted);
+        }
+
+        public static byte[] Compress(string str)
+        {
+            int byteLength = str.Length / 4, byteNumber = 2;
+            if (str.Length % 4 != 0)
+            {
+                byteLength++;
+            }
+
+            // резервуємо 2 біта для збереження довжини оригінального рядка
+            byteLength += 2;
+            byte[] bytes = new byte[byteLength];
+
+            // записуємо довжину оригінального рядка
+            bytes[0] = (byte)(str.Length >> 8);
+            bytes[1] = (byte)(str.Length & 255);
+            int i = 0;
+            while (i < str.Length)
+            {
+                int j = 0;
+                while (j < 4 && i < str.Length)
+                {
+                    switch (str[i])
+                    {
+                        case 'A':
+                            bytes[byteNumber] |= 0b00;
+                            break;
+                        case 'C':
+                            bytes[byteNumber] |= 0b01;
+                            break;
+                        case 'G':
+                            bytes[byteNumber] |= 0b10;
+                            break;
+                        case 'T':
+                            bytes[byteNumber] |= 0b11;
+                            break;
+                    }
+                    // останній байт не повинен зсуватися
+                    bytes[byteNumber] <<= j == 3 ? 0 : 2;
+                    j++;
+                    i++;
+                }
+                byteNumber++;
+            }
+            return bytes;
+        }
+
+        public static string Decompress(byte[] bytes)
+        {
+            StringBuilder sb = new StringBuilder();
+            // читаємо довжину оригінального рядка
+            int length = (bytes[0] << 8) + bytes[1];
+            for (int i = 2; i < bytes.Length; i++)
+            {
+                for (int j = 0; j < 4 && length > 0; j++)
+                {
+                    switch (bytes[i] >> 6)
+                    {
+                        case 0b00:
+                            sb.Append('A');
+                            break;
+                        case 0b01:
+                            sb.Append('C');
+                            break;
+                        case 0b10:
+                            sb.Append('G');
+                            break;
+                        case 0b11:
+                            sb.Append('T');
+                            break;
+                    }
+                    bytes[i] <<= 2;
+                    length--;
+                }
+            }
+            return sb.ToString();
+        }
+
+        public static void Task5()
+        {
+            Console.WriteLine("Begin Task 5");
+            Console.WriteLine("Enter string");
+            string str = Console.ReadLine();
+            if (str == null)
+            {
+                Console.WriteLine("String is null");
+                return;
+            }
+
+            byte[] bytes = Compress(str);
+            string str2 = Decompress(bytes);
+
+
+            Console.WriteLine(str2.ToString());
+        }   
+
+        public static void Task4()
+        {
+            Console.WriteLine("Begin Task 4");
+            Console.WriteLine("Enter numbers");
+            string str = Console.ReadLine();
+            
+            //example: "3 5 4 7 1  0 6" so need to find "2"
+            
+            if (str == null)
+            {
+                Console.WriteLine("String is null");
+                return;
+            }
+            int num;
+            string[] strNumbers = str.Split(' ');
+           
+            int sum = 0, sumCorrect = 0;
+            for (int i = 0; i < strNumbers.Length; i++)
+            {
+                sumCorrect += i;
+                if (int.TryParse(strNumbers[i], out num))
+                {
+                    sum += num;
+                }
+            }
+
+            Console.WriteLine(sumCorrect-sum);
+        }
+
+        public static void Task3()
+        {
+            Console.WriteLine("Begin Task 3");
+            Console.WriteLine("Enter number");
+            string str = Console.ReadLine();
+
+            if (str == null)
+            {
+                Console.WriteLine("String is null");
+                return;
+            }
+            int number = int.Parse(str);
+
+            var sb = new StringBuilder();
+
+            for (int i = 0; i < number; i++)
+            {
+                // Печатні символи від 32 до 126
+                var seed = Math.Abs(Environment.TickCount << i);
+                var letterNumber = (32 + (seed) % (126 - 32));
+                sb.Append((char)letterNumber);
+            }
+            Console.WriteLine(sb.ToString());
+        }
+
+        public static void Task2()
+        {
+            Console.WriteLine("Begin Task 2");
+            Console.WriteLine("Enter string");
+            string str = Console.ReadLine();
+
+            if (str == null)
+            {
+                Console.WriteLine("String is null");
+                return;
+            }
+            Console.WriteLine("Enter exceptions devided by SPACE");
+
+            string exceptionsStr = Console.ReadLine();
+
+            if (exceptionsStr == null)
+            {
+                Console.WriteLine("Exceptions is null");
+                return;
+            }
+            string[] exceptions = exceptionsStr.Split(' ');
+            
+            Array.Sort(exceptions);
+
+            StringBuilder sb = new StringBuilder();
+            bool isFirstLetter = char.IsLetter(str[0]);
+            int wordLength = 0;
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (char.IsLetter(str[i]) && isFirstLetter)
+                {
+                    isFirstLetter = false;
+                    wordLength = 1;
+                }
+                else if (char.IsLetter(str[i]) && !isFirstLetter)
+                {
+                    wordLength++;
+                }
+                if (!char.IsLetter(str[i]) && !isFirstLetter)
+                {
+                    isFirstLetter = true;
+                    string word = str.Substring(i - wordLength, wordLength);
+                    if (Array.BinarySearch(exceptions, word) < 0)
+                    {
+                        sb.Append(word);
+                        sb.Append(' ');
+                    }
+                    else
+                    {
+                        sb.Append("***");
+                        sb.Append(' ');
+                    }
+                }
+
+                if (i == str.Length - 1)
+                {
+                    isFirstLetter = true;
+                    string word = str.Substring(str.Length - wordLength, wordLength);
+                    if (Array.BinarySearch(exceptions, word) < 0)
+                    {
+                        sb.Append(word);
+                        sb.Append(' ');
+                    }
+                    else
+                    {
+                        sb.Append("***");
+                        sb.Append(' ');
+                    }
+                }
+            }
+
+            Console.WriteLine(sb);
+
+        }
+
+        public static void Task1()
+        {
+            Console.WriteLine("Begin Task 1");
+            string str = Console.ReadLine();
+
+            if (str == null)
+            {
+                Console.WriteLine("String is null");
+                return;
+            }
+
+            char[] arr = new char[str.Length];
+            for (int i = 0; i < arr.Length/2; i++)
+            {
+                char temp = arr[i];
+                arr[i] = arr[arr.Length - 1 - i];
+                arr[arr.Length -1 - i] = temp;
+            }
+
+            Console.WriteLine(arr);
+            Console.WriteLine("End Task 1");
+        }
+    }
+}
