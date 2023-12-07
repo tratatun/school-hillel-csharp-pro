5a4f3a6626c86d1ebc5d51453d420d489c7fe4d9
Oleksandr Nikiforov <nikiforov.aleksandr@gmail.com>
2023-12-07 02:27:32 -0800

    initial Black Jack

---
+++ Lesson05/Lesson05/Classes/CardDeck.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson05.Classes
+{
+    public class CardDeck
+    {
+        public List<Card> Cards = new List<Card>();
+
+        public CardDeck()
+        {
+            foreach (CardSuit suit in Enum.GetValues(typeof(CardSuit)))
+            {
+                foreach (CardValue value in Enum.GetValues(typeof(CardValue)))
+                {
+                    Cards.Add(new Card { Suit = suit, Value = value });
+                }
+            }
+        }
+
+        public void Shuffle()
+        {
+            Random random = new Random();
+            Cards = Cards.OrderBy(c => random.Next()).ToList();
+        }
+
+        public void Sort()
+        {
+            Cards = Cards.OrderBy(c => c.Suit).ThenBy(c => c.Value).ToList();
+        }
+
+        public Card GetCard()
+        {
+            Card card = Cards[0];
+            Cards.RemoveAt(0);
+            return card;
+        }
+
+        public void Print()
+        {
+            foreach (var card in Cards)
+            {
+                Console.WriteLine($"{card.Value} of {card.Suit}");
+            }
+        }
+
+
+
+    }
+
+    public struct Card
+    {
+        public CardSuit Suit { get; set; }
+        public CardValue Value { get; set; }
+    }
+    
+    public enum CardSuit
+    {         
+        Clubs,
+        Diamonds,
+        Hearts,
+        Spades
+    }
+
+    public enum CardValue
+    {
+        //Two, Three, Four, Five, 
+        Six = 6, Seven, Eight, Nine, Ten,
+        Jack, Queen, King, Ace
+    }
+}
---
+++ Lesson05/Lesson05/Program.cs
@@ -0,0 +1,64 @@
+﻿using Lesson05.Classes;
+
+namespace Lesson05
+{
+    internal class Program
+    {
+        // Game "21"
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            var cardDeck = new CardDeck();
+
+            Console.WriteLine("Shuffle deck");
+            cardDeck.Shuffle();
+            PrintAcesPositions(cardDeck);
+            cardDeck.Print();
+
+            Console.WriteLine("Moving spades to top");
+            cardDeck = MoveSpadesToTop(cardDeck);
+            cardDeck.Print();
+
+            Console.WriteLine("Sort deck");
+            cardDeck.Sort();
+            cardDeck.Print();
+
+            Console.WriteLine("Let's play");
+            var game = new GameBlackJack();
+            game.Play();
+
+        }
+
+
+        public static void PrintAcesPositions(CardDeck deck)
+        {
+            for (int i = 0; i < deck.Cards.Count; i++)
+            {
+                if (deck.Cards[i].Value == CardValue.Ace)
+                {
+                    Console.WriteLine($"Card {deck.Cards[i].Value} of {deck.Cards[i].Suit} is at position {i}");
+                }
+            }
+        }
+
+        public static CardDeck MoveSpadesToTop(CardDeck deck)
+        {
+            var newDeck = new CardDeck();
+            int spadesCount = 0;
+            foreach (var card in deck.Cards)
+            {
+                if (card.Suit == CardSuit.Spades)
+                {
+                    newDeck.Cards.Insert(spadesCount++, card);
+                }
+                else
+                {
+                    newDeck.Cards.Add(card);
+                }
+            }
+
+            return newDeck;
+        }
+
+    }
+}
