c7dbaa8b4ee38fc166f31f7eb1ae41953442223b
Oleksandr Nikiforov <nikiforov.aleksandr@gmail.com>
2023-12-10 16:44:58 -0800

    refactoring

--- Lesson05/Lesson05/Classes/CardDeck.cs
+++ Lesson05/Lesson05/Classes/CardDeck.cs
@@ -8,7 +8,7 @@ namespace Lesson05.Classes
 {
     public class CardDeck
     {
-        public List<Card> Cards = new List<Card>();
+        private List<Card> _cards = new List<Card>();
 
         public CardDeck()
         {
@@ -16,7 +16,7 @@ namespace Lesson05.Classes
             {
                 foreach (CardValue value in Enum.GetValues(typeof(CardValue)))
                 {
-                    Cards.Add(new Card { Suit = suit, Value = value });
+                    _cards.Add(new Card ( suit, value));
                 }
             }
         }
@@ -24,37 +24,76 @@ namespace Lesson05.Classes
         public void Shuffle()
         {
             Random random = new Random();
-            Cards = Cards.OrderBy(c => random.Next()).ToList();
+            _cards = _cards.OrderBy(c => random.Next()).ToList();
         }
 
         public void Sort()
         {
-            Cards = Cards.OrderBy(c => c.Suit).ThenBy(c => c.Value).ToList();
+            _cards = _cards.OrderBy(c => c.Suit).ThenBy(c => c.Value).ToList();
         }
 
-        public Card GetCard()
+        public Card GetCard(int index = 0)
         {
-            Card card = Cards[0];
-            Cards.RemoveAt(0);
+            Card card = _cards[index];
+            _cards.RemoveAt(index);
             return card;
         }
 
         public void Print()
         {
-            foreach (var card in Cards)
+            foreach (var card in _cards)
             {
                 Console.WriteLine($"{card.Value} of {card.Suit}");
             }
         }
 
+        public int GetCardsCount()
+        {
+            return _cards.Count;
+        }
 
+        public void PrintAcesPositions()
+        {
+            for (int i = 0; i < GetCardsCount(); i++)
+            {
+                if (GetCard(i).Value == CardValue.Ace)
+                {
+                    Console.WriteLine($"Card {GetCard(i).Value} of {GetCard(i).Suit} is at position {i}");
+                }
+            }
+        }
+
+        public void MoveSpadesToTop()
+        {
+            var newDeck = new CardDeck();
+            int spadesCount = 0;
+            foreach (var card in _cards)
+            {
+                if (card.Suit == CardSuit.Spades)
+                {
+                    newDeck._cards.Insert(spadesCount++, card);
+                }
+                else
+                {
+                    newDeck._cards.Add(card);
+                }
+            }
+
+            _cards = newDeck._cards;
+        }
 
     }
 
     public struct Card
     {
-        public CardSuit Suit { get; set; }
-        public CardValue Value { get; set; }
+        public CardSuit Suit { get; }
+        public CardValue Value { get; }
+
+        public Card(CardSuit cardSuit, CardValue cardValue)
+        {
+            Suit = cardSuit;
+            Value = cardValue;
+        }
     }
     
     public enum CardSuit
--- Lesson05/Lesson05/Program.cs
+++ Lesson05/Lesson05/Program.cs
@@ -12,11 +12,11 @@ namespace Lesson05
 
             Console.WriteLine("Shuffle deck");
             cardDeck.Shuffle();
-            PrintAcesPositions(cardDeck);
+            cardDeck.PrintAcesPositions();
             cardDeck.Print();
 
             Console.WriteLine("Moving spades to top");
-            cardDeck = MoveSpadesToTop(cardDeck);
+            cardDeck.MoveSpadesToTop();
             cardDeck.Print();
 
             Console.WriteLine("Sort deck");
@@ -28,37 +28,5 @@ namespace Lesson05
             game.Play();
 
         }
-
-
-        public static void PrintAcesPositions(CardDeck deck)
-        {
-            for (int i = 0; i < deck.Cards.Count; i++)
-            {
-                if (deck.Cards[i].Value == CardValue.Ace)
-                {
-                    Console.WriteLine($"Card {deck.Cards[i].Value} of {deck.Cards[i].Suit} is at position {i}");
-                }
-            }
-        }
-
-        public static CardDeck MoveSpadesToTop(CardDeck deck)
-        {
-            var newDeck = new CardDeck();
-            int spadesCount = 0;
-            foreach (var card in deck.Cards)
-            {
-                if (card.Suit == CardSuit.Spades)
-                {
-                    newDeck.Cards.Insert(spadesCount++, card);
-                }
-                else
-                {
-                    newDeck.Cards.Add(card);
-                }
-            }
-
-            return newDeck;
-        }
-
     }
 }
